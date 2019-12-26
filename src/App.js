acd7563f7c12399eb06554d4c6d0aa0e9723acc2
María Simó <maria@coponstudio.es>
2019-12-26 16:25:13 +0100

    Installed sass and added webfont

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
-import './App.css';
+import './App.scss';
 
 const strapi = new Strapi('http://localhost:1337');
 // const strapi = new Strapi(`${process.env.REACT_APP_API_URL}`);
@@ -24,9 +24,13 @@ class App extends React.Component {
     console.log(this.state)
     return (
       <div className='App'>
+        <p>
+        Copón Studio is a creative couple 🐈🐈 working from sunny Murcia — Spain (mostly) on graphic + digital design projects. Our portfolio includes brand & identity design, ✏ illustration, user interface 👁 design and web development . We like to think at design and code as a ⁂ craftmanship ⁂ and to 💬 work next to people (clients, partners & colleagues) to achieve best results. 
+        </p>
         <section>
           {this.state.projects.map(project => 
             <article key={project.id}>
+              <h1>{project.Title}</h1>
               <img src={`http://localhost:1337${project.featureImage.url}`} style={{maxWidth:'300px'}}/>
             </article>  
           )}
